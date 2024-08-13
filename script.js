8118dfbb0909aa2bee0b545a91df30e5e0ee39f6
vitorlealf <104856199+vitorlealf@users.noreply.github.com>
2024-08-13 09:12:56 -0300

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,125 @@
-const caixaPrincipal = document.querySelector(".caixa-prinncipal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+
 const perguntas = [
     {
-        enunciado: "No coração de uma floresta densa e esquecida pelo tempo, erguia-se uma casa antiga e abandonada. Suas paredes de madeira apodrecida rangiam ao vento noturno, e suas janelas quebradas pareciam olhos vazios observando o mundo exterior com um ar de mistério e desolação. Havia uma aura de medo e segredos sinistros que pairava sobre aquele lugar, conhecido pelos moradores locais apenas como A Casa do Medo. Na noite em questão, você, um explorador curioso em busca de emoções fortes, decide desafiar os rumores sobre a casa. Sob o brilho pálido da lua cheia, você se aproxima cautelosamente da entrada, cuja porta range ao abrir com um som estridente que ecoa pela floresta. Ao entrar, a escuridão envolve você como um manto gélido. O ar está impregnado com um cheiro de mofo e algo indescritivelmente podre. Seu coração bate acelerado, mas a curiosidade alimenta sua coragem. A única fonte de luz é a lanterna tremeluzente que você segura firme em uma das mãos. Subitamente, um som gutural ecoa das sombras além da escada que leva ao andar superior da casa. Uma voz rouca, quase sussurrante, pergunta: Quem ousa perturbar meu sono?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Eu não quero problemas. Estou apenas de passagem.",
+                texto: "Isso é assustador!",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Mostre-se! Eu não tenho medo de fantasmas!",
+                texto: "Isso é maravilhoso!",
                 afirmacao: "afirmação"
             }
         ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+];
+
+
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
- ];
- 
- 
- let atual = 0;
- let perguntaAtual;
- 
- 
- function mostrapergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    };
- 
- 
- mostrapergunta();
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+
+mostraPergunta();
+
  
  
  
